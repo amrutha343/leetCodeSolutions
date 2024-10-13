231daa1cba8f8903dcc9c27a72445fa1163f21a1
Amrutha Sujith <amrutha343@gmail.com>
2024-10-13 22:03:30 +0530

    Add files via upload
    242. Valid Anagram
    Solution Count = 1
    Primary Datastructure used = Hashmap

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,54 @@
+package com.helloworld;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class HelloWorld {
+
+	/* Driver program to test above functions */
+	 public static void main(String args[]) 
+	 { 
+		 Solution s = new Solution();
+		 String st = "ccac";
+		 String t = "ccac";
+		 System.out.println(s.isAnagram(st, t));
+	 } 
+}
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    	int count =0;
+        Map<Character,Integer> anagramMap = new HashMap<>();
+        if(s.length()!=t.length()) {
+        	return false;
+        }
+        for(int i=0;i<s.length();i++) {
+        	if(anagramMap.containsKey(s.charAt(i))) {
+        		anagramMap.put(s.charAt(i),anagramMap.get(s.charAt(i))+1);
+        	}else {
+        		anagramMap.put(s.charAt(i),1);
+        	}
+        	System.out.println(anagramMap);
+        }
+        for(int i=0;i<t.length();i++) {
+        	if(anagramMap.containsKey(t.charAt(i))) {
+        		anagramMap.put(t.charAt(i),anagramMap.get(s.charAt(i))-1);
+        	}else {
+        		return false;
+        	}
+        	System.out.println(anagramMap);
+        }
+        Set<Character> anagramSet = new HashSet<>();
+        anagramSet = anagramMap.keySet();
+        
+        for(Character key:anagramSet) {
+        	if(anagramMap.get(key)!=0) {
+        		return false;
+        	}
+        }
+        return true;
+    }
+}
+ 
